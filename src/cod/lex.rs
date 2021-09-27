c0bc93a558091142467b82bddb43f719af37f9d4
six-day_tech <9695992+six-day_tech@user.noreply.gitee.com>
2021-09-27 20:12:17 +0800

    9-27

--- src/cod/lex.rs
+++ src/cod/lex.rs
@@ -130,7 +130,64 @@ pub fn lex(input: &str) -> Vec<Token> {
                 }
             }
             // keyword identifier constant stringliteral
-            
+            _ => {
+                if c.is_alphabetic() {
+                    let mut s = c.to_string();
+
+                    loop {
+                        match input.peek() {
+                            Some(&a) if a.is_alphanumeric() || a == '_' => s.push(a),
+                            _ => break,
+                        }
+                        input.next();
+                    }
+
+                    match &s[..] {
+                        "auto" => tokens.push(Token::Keyword(Keyword::Auto)),
+                        "double" => tokens.push(Token::Keyword(Keyword::Double)),
+                        "int" => tokens.push(Token::Keyword(Keyword::Int)),
+                        "struct" => tokens.push(Token::Keyword(Keyword::Struct)),
+                        "break" => tokens.push(Token::Keyword(Keyword::Break)),
+                        "else" => tokens.push(Token::Keyword(Keyword::Else)),
+                        "long" => tokens.push(Token::Keyword(Keyword::Long)),
+                        "switch" => tokens.push(Token::Keyword(Keyword::Switch)),
+                        "char" => tokens.push(Token::Keyword(Keyword::Char)),
+                        "extern" => tokens.push(Token::Keyword(Keyword::Extern)),
+                        "return" => tokens.push(Token::Keyword(Keyword::Return)),
+                        "union" => tokens.push(Token::Keyword(Keyword::Union)),
+                        "const" => tokens.push(Token::Keyword(Keyword::Const)),
+                        "float" => tokens.push(Token::Keyword(Keyword::Float)),
+                        "short" => tokens.push(Token::Keyword(Keyword::Short)),
+                        "unsigned" => tokens.push(Token::Keyword(Keyword::Unsigned)),
+                        "continue" => tokens.push(Token::Keyword(Keyword::Continue)),
+                        "for" => tokens.push(Token::Keyword(Keyword::For)),
+                        "signed" => tokens.push(Token::Keyword(Keyword::Signed)),
+                        "void" => tokens.push(Token::Keyword(Keyword::Void)),
+                        "default" => tokens.push(Token::Keyword(Keyword::Default)),
+                        "goto" => tokens.push(Token::Keyword(Keyword::Goto)),
+                        "sizeof" => tokens.push(Token::Keyword(Keyword::Sizeof)),
+                        "voiatile" => tokens.push(Token::Keyword(Keyword::Voiatile)),
+                        "do" => tokens.push(Token::Keyword(Keyword::Do)),
+                        "if" => tokens.push(Token::Keyword(Keyword::If)),
+                        "static" => tokens.push(Token::Keyword(Keyword::Static)),
+                        "while" => tokens.push(Token::Keyword(Keyword::While)),
+                        _ => tokens.push(Token::Identifier(s)),
+                    }
+                } else if c.is_digit(10) {
+                    let mut n = c.to_string();
+
+                    loop {
+                        match input.peek() {
+                            Some(c) if c.is_digit(10) => n.push(*c),
+                            _ => break,
+                        }
+                        input.next();
+                    }
+
+                    let n = n.parse::<i32>().unwrap();
+                    tokens.push(Token::Constant(n));
+                }
+            }
         }
     }
 
